c60bea5e98da4f0720b13fbecd90997116d2716e
Maximiliano Friss <mfrissdekereki@gmail.com>
2022-11-12 15:05:28 -0300

    Se modifica nombre de main.js a server.js, se agregan lineas de express para poder pasar archivos json por body

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const multer = require('multer')
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 const Contenedor = require('./classContenedor.js')
 
 const televisor = {
